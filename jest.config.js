31b4e09f6acfa192874ade11728c51841f90c3c4
Loreerol <lorehill.hello@gmail.com>
2026-04-02 14:42:34 +0200

    update config

--- jest.config.js
+++ jest.config.js
@@ -10,10 +10,10 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts', '!src/settings.ts'],
   coverageThreshold: {
     global: {
-      branches: 90,
-      functions: 90,
-      lines: 90,
-      statements: 90,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
 };
